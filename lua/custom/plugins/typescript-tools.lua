9660dbaf8a7a7aef7eb22d53a1e908840679d008
Pawel Fras <fras.pawel@yahoo.com>
2024-04-09 23:23:23 +0200

    adjust ts-tools and none-ls plugins

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -1,12 +1,13 @@
 return {
   'pmizio/typescript-tools.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {},
   config = function()
     require('typescript-tools').setup {
       on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
+        client.server_capabilities.renameProvider = false
       end,
       vim.keymap.set('n', '<leader>ci', ':TSToolsAddMissingImports<CR>', { desc = 'Add Missing [I]Imports' }),
       vim.keymap.set('n', '<leader>co', ':TSToolsOrganizeImports<CR>', { desc = '[O]rganize Imports' }),
