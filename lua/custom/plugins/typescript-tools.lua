f1938cddbf573d48f0c22d2a325016d1ed3e659f
Pawel Fras <fras.pawel@yahoo.com>
2024-04-09 23:40:34 +0200

    adjust ts-tools to fix duplicated signatures

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -8,6 +8,7 @@ return {
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
         client.server_capabilities.renameProvider = false
+        client.server_capabilities.signatureHemlProvider = false
       end,
       vim.keymap.set('n', '<leader>ci', ':TSToolsAddMissingImports<CR>', { desc = 'Add Missing [I]Imports' }),
       vim.keymap.set('n', '<leader>co', ':TSToolsOrganizeImports<CR>', { desc = '[O]rganize Imports' }),
