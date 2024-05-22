800e0cd1e97935cf28eb271b0421f3e2ac4b66cd
Paweł Fraś <fras.pawel@yahoo.com>
2024-05-22 22:57:24 +0200

    adjust debugging.lua

--- lua/custom/plugins/debugging.lua
+++ lua/custom/plugins/debugging.lua
@@ -2,9 +2,22 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'mxsdev/nvim-dap-vscode-js',
+    'microsoft/vscode-js-debug',
   },
   config = function()
-    local dap, dapui = require 'dap', require 'dapui'
+    local dap, dapui, dap_vscode_js = require 'dap', require 'dapui', require 'dap-vscode-js'
+
+    dap_vscode_js.setup {
+      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      debugger_path = os.getenv 'HOME' .. '/.local/share/nvim/lazy/vscode-js-debug', -- Path to vscode-js-debug installation.
+      -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    }
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -57,5 +70,39 @@ return {
       local widgets = require 'dap.ui.widgets'
       widgets.centered_float(widgets.scopes)
     end)
+
+    for _, language in ipairs { 'typescript', 'javascript' } do
+      dap.configurations[language] = {
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-node',
+          request = 'attach',
+          name = 'Attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Debug Jest Tests',
+          -- trace = true, -- include debugger info
+          runtimeExecutable = 'node',
+          runtimeArgs = {
+            './node_modules/jest/bin/jest.js',
+            '--runInBand',
+          },
+          rootPath = '${workspaceFolder}',
+          cwd = '${workspaceFolder}',
+          console = 'integratedTerminal',
+          internalConsoleOptions = 'neverOpen',
+        },
+      }
+    end
   end,
 }
